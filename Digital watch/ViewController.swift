01263d82e2ea6327f04f83bff1d170aaf0770ad8
D7703_28 <d7703_28@d7703-28ui-imac.local>
2018-04-10 15:20:52 +0900

    스위치 시계

--- Digital watch/ViewController.swift
+++ Digital watch/ViewController.swift
@@ -1,25 +1,42 @@
-//
-//  ViewController.swift
-//  Digital watch
-//
-//  Created by D7703_28 on 2018. 4. 10..
-//  Copyright © 2018년 YDuChoi. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var timerSwitch: UISwitch!
+    var myTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // switch off
+        timerSwitch.setOn(false, animated: true)
+        
+        //        myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func updateTime() {
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss a"
+        timeLabel.text = formatter.string(from: date)
+        //        timeLabel.text = String(describing: date)
     }
-
-
+    
+    @IBAction func timerStrat(_ sender: Any) {
+        if timerSwitch.isOn == true {
+            //            myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+            
+            // closure 사용
+            myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTimer) in
+                self.updateTime()
+            })
+            
+        } else {
+            myTimer.invalidate()
+            
+        }
+    }
 }
 
